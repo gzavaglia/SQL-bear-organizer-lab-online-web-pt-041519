ea3a0e555c670e6536588da0a58ddf97fd6dc0e1
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-06-11 18:26:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,16 +1,9 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Mr. Chocolate", 5, "Male", "Brown", "OK", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Rowdy", 2, "Male", "Brown", "OK", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Tabitha", 22, "Female", "Brown", "OK", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Sargeant Brown", 12, "Female", "Brown", "OK", 0);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Melissa", 11, "Fluid", "Brown", "OK", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Grinch", 3, "Fluid", "blue", "very arrr", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES ("Wendy", 22, "Male", "pink", "gay", 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive)
-    VALUES (NULL, 2, "Male", "Brown", "OK", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES
+(1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 0),
+(2,"Rowdy", 10, "M", "black", "intense", 1),
+(3,"Tabitha", 6, "F", "dark brown", "Nice", 1),
+(4,"Sargeant Brown", 19, "M", "Green", "Slimy", 0),
+(5,"Melissa", 13, "F", "dark brown", "goofy", 1),
+(6,"Grinch", 2, "M", "Black", "Grinchy", 1),
+(7,"Wendy", 6, "F", "Blue", "naive", 1),
+(8,null, 20, "M", "black", "aggressive", 0);
